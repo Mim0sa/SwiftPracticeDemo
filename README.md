ed921b467b7de76b2f404879a1aad39d1fc94708
Mim0sa <mim0sa@qq.com>
2018-11-19 23:11:22 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PublishPage
-一个Swift版的提交/发布页面（文字/图片/语音）/A Publish Page made by Swift
+一些学习的 Swift 练手 demo
---
+++ StretchView_demo/StretchView_demo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StretchView_demo
+//
+//  Created by 吉乞悠 on 2018/11/19.
+//  Copyright © 2018 吉乞悠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    var selectedStretchView:StretchView?
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if selectedStretchView != nil {
+//            selectedStretchView?.shrinkTheView()
+//        }
+//    }
+    
+}
+
