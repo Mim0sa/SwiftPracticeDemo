ddbdeb53f8ecbbe95cd80b77d7bcf541bb81e31d
Mim0sa <mim0sa@qq.com>
2018-12-01 11:34:33 +0800

    update

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -46,13 +46,14 @@ class ViewController: UIViewController {
         }
     }
     
-    private var emojiChoices = ["🦇","😱","🙀","👿","🎃","👻","🍭","🍬","🍎"]
+    private var emojiChoices = "🦇😱🙀👿🎃👻🍭🍬🍎"
     
     private var emoji = [Card:String]()
     
     private func emoji(for card: Card) -> String {
         if emoji[card] == nil, emojiChoices.count > 0 {
-            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
         return emoji[card] ?? "?"
     }
