60fe98e1253364270899af5de7a558d06f16825c
Mim0sa <mim0sa@qq.com>
2018-12-07 22:53:00 +0800

    initial commit

---
+++ WhatToEat/WhatToEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToEat
+//
+//  Created by 吉乞悠 on 2018/12/7.
+//  Copyright © 2018 吉乞悠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
