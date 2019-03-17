5ff0a491b4354d794826e4af62448c3ed7bfd52d
Mim0sa <mim0sa@qq.com>
2019-03-17 21:37:06 +0800

    update

---
+++ WWDC19/Swift_Playgrounds_Author_Template_for_Xcode_10/Template/PlaygroundBook/Chapters/Chapter2.playgroundchapter/Pages/PlaygroundPage.playgroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+/*:
+**Challenge:** This is a demonstration of Bubble Sort procedures and the soldiers will be ranged from low to high
+
+Use BubbleSorting() to start the demo
+
+[You may want to stand the ipad on the end]
+
+Click here to jump to [The Next Page](@next)
+*/
+import UIKit
+
+let str = "Hello, playground"
+
