f8fc2fecec1a393a041958fd96f9b1f60cf8c429
Mim0sa <mim0sa@qq.com>
2019-08-24 10:51:02 +0800

    update

--- Creator/Creator/View/ProgramBoardTableViewCell.swift
+++ Creator/Creator/View/ProgramBoardTableViewCell.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ProgramBoardTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        selectionStyle = .none
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
 }
