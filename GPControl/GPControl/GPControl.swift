7223e85a1122f0bbb7a38baaba70dd0b1f464470
Mim0sa <mim0sa@qq.com>
2020-03-21 23:29:38 +0800

    GPControl initial

---
+++ GPControl/GPControl/GPControl.swift
@@ -0,0 +1,18 @@
+//
+//  GPControl.swift
+//  GPControl
+//
+//  Created by 吉乞悠 on 2020/3/21.
+//  Copyright © 2020 吉乞悠. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GPControl {
+    
+    func addGamePaddleArea() {
+        
+    }
+    
+}
