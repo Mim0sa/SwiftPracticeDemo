3b2ef97eea7a225d3cb2a8539eabd8287691979b
Mim0sa <mim0sa@qq.com>
2020-05-22 15:12:54 +0800

    update

--- ProtoPipe/ProtoPipe/MagicMoveAnimator.swift
+++ ProtoPipe/ProtoPipe/MagicMoveAnimator.swift
@@ -15,27 +15,19 @@ class MagicMoveAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        //let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
         let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
         let containerView = transitionContext.containerView
         
-        //var fromView = fromViewController?.view
         var toView = toViewController?.view
-        
         if transitionContext.responds(to: #selector(UIViewControllerTransitionCoordinatorContext.view(forKey:))) {
-            //fromView = transitionContext.view(forKey: UITransitionContextViewKey.from)
             toView = transitionContext.view(forKey: UITransitionContextViewKey.to)
         }
         
-        //fromView?.frame = transitionContext.initialFrame(for: fromViewController!)
         toView?.frame = transitionContext.finalFrame(for: toViewController!)
-        
-        //fromView?.alpha = 1.0
         toView?.alpha = 0.0
         
         containerView.addSubview(toView!)
-        
-        //
+
         let tempView = UIImageView()
         tempView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
         tempView.backgroundColor = .orange
@@ -44,15 +36,13 @@ class MagicMoveAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         let transitionDuration = self.transitionDuration(using: transitionContext)
         UIView.animate(withDuration: transitionDuration, animations: {
             tempView.frame = transitionContext.finalFrame(for: toViewController!)
-            //fromView!.alpha = 0.0
-            //toView!.alpha = 1.0
         }) { (finished: Bool) -> Void in
             tempView.alpha = 0
             toView!.alpha = 1.0
+            
             let wasCancelled = transitionContext.transitionWasCancelled
             transitionContext.completeTransition(!wasCancelled)
         }
-        UIView.animateKeyframes(withDuration: <#T##TimeInterval#>, delay: <#T##TimeInterval#>, options: <#T##UIView.KeyframeAnimationOptions#>, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
     }
     
 }
