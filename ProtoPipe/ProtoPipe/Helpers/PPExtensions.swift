21ee296fb9b9eaf769c04587af0d9eb88ae99f05
mimosa <563576965@qq.com>
2020-06-17 17:55:49 +0800

    update

--- ProtoPipe/ProtoPipe/Helpers/PPExtensions.swift
+++ ProtoPipe/ProtoPipe/Helpers/PPExtensions.swift
@@ -10,9 +10,9 @@ import UIKit
 
 // MARK: - UIColor
 extension UIColor {
-    static let sceneBlack = UIColor(withHex: 0x191B1D)
-    static let navigatorBlack = UIColor(withHex: 0x131415)
-    static let activeGreen = UIColor(withHex: 0x50BAA1)
-    static let titleWhite = UIColor(withHex: 0xeeeeee)
-    static let subtitleGray = UIColor(withHex: 0xcccccc)
+    static let sceneBlack       = UIColor(withHex: 0x191B1D)
+    static let navigatorBlack   = UIColor(withHex: 0x131415)
+    static let activeGreen      = UIColor(withHex: 0x50BAA1)
+    static let titleWhite       = UIColor(withHex: 0xeeeeee)
+    static let subtitleGray     = UIColor(withHex: 0xcccccc)
 }
