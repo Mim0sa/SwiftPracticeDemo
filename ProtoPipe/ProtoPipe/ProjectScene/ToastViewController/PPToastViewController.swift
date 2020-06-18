e18f9c89c7a32d46be50a7405fc88aa0c597c07d
mimosa <563576965@qq.com>
2020-06-18 17:59:24 +0800

    arrange code

--- ProtoPipe/ProtoPipe/ProjectScene/ToastViewController/PPToastViewController.swift
+++ ProtoPipe/ProtoPipe/ProjectScene/ToastViewController/PPToastViewController.swift
@@ -21,7 +21,7 @@ struct PPToastModel {
 class PPToastViewController: PPBaseViewController, PPToastViewDelegate {
     
     let toastNavigationBar = PPToastNavigationBar()
-    let toastView : PPToastView!
+    let toastView: PPToastView
     
     var toastModel: PPToastModel
     
@@ -31,17 +31,17 @@ class PPToastViewController: PPBaseViewController, PPToastViewDelegate {
         self.toastModel = toastModel
         self.toastView = toastModel.toastView
         super.init(nibName: nil, bundle: nil)
-
+        
         toastNavigationBar.title = toastModel.title
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         modalPresentationStyle = .pageSheet
         
         view.backgroundColor = .sceneBlack
@@ -58,7 +58,7 @@ class PPToastViewController: PPBaseViewController, PPToastViewDelegate {
             make.top.equalTo(toastNavigationBar.snp.bottom)
         }
     }
-
+    
 }
 
 extension PPToastViewController {
