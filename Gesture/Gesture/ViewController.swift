0fb5a23fef2c803183bd4ac58d1d5529a435c3f5
mimosa <563576965@qq.com>
2021-07-12 10:00:58 +0800

    add Gesture

---
+++ Gesture/Gesture/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Gesture
+//
+//  Created by Mimosa on 2021/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnClicked(_ sender: UIButton) {
+        print("Clicked")
+    }
+    
+}
+
